61dfa90fbf817a37d064e9e16cfffaffb62a1d99
Adama NIADA <a77272199@gmail.com>
2023-03-15 18:39:45 +0000

    Commit 4.0

--- resources/views/layouts/include/navbar.blade.php
+++ resources/views/layouts/include/navbar.blade.php
@@ -1,42 +1,27 @@
-{{-- <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm"> --}}
-{{-- <nav class="navbar navbar-expand-lg bg-light rounded shadow-sm" aria-label="Twelfth navbar example">
+<nav class="navbar shadow" style="background-color:#e3f2fd;">
     <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample10" aria-controls="navbarsExample10" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbarDark" aria-controls="offcanvasNavbarDark">
             <span class="navbar-toggler-icon"></span>
         </button>
-
-        <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample10">
-            <ul class="navbar-nav">
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('blog') }}">BLOG</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('oss') }}">OSS</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('projet') }}">PROJETS</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('about') }}">ABOUT</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
-<br> --}}
-<nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
-    <div class="container-fluid">
-        {{-- <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a> --}}
-        <a class="navbar-brand" href="{{ url('/') }}">LOGO ICI</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbarLight" aria-controls="offcanvasNavbarLight">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbarLight" aria-labelledby="offcanvasNavbarLightLabel">
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbarDark" aria-labelledby="offcanvasNavbarDarkLabel">
             <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasNavbarLightLabel">MENU</h5>
+                <h5 class="offcanvas-title" id="offcanvasNavbarDarkLabel">MENU</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div class="offcanvas-body">
                 <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    {{-- <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Home</a></li> --}}
                     <li class="nav-item"><a class="nav-link active" aria-current="page" href="{{ url('/') }}">PORTFOLIO</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('blog') }}">BLOG</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('oss') }}">OSS</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('projet') }}">PROJETS</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('about') }}">ABOUT</a></li>
                 </ul>
+                {{-- <form class="d-flex mt-3" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form> --}}
             </div>
         </div>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,11 +16,52 @@
             @endif
         </div>
 
-        <div class="col-md-8 mb-5">
+        <div class="col-md-10 mb-5">
             <div class="card shadow-sm">
                 <div class="card-body">
-                    <h4 class="card-title">{{ __('PORTFOLIO') }}</h4>
-                    <div class="card-text">Section en cours de developpement</div>
+                    <div class="alert alert-primary" role="alert">
+                        <div class="alert-heading h4 d-flex justify-content-between align-items-center">
+                            <div>Adama NIADA</div>
+                            <div>
+                                <div><a href="mailto:adama2mg@gmail.com" class="underline:none">adama2mg@gmail.com</a></div>
+                                <div><a href="tel:+22621456578">+22621456578</a></div>
+                            </div>
+                        </div>
+                        <hr>
+                        <div>
+                            <span><a class="underline:none" href="https://www.linkedin.com/in/adama-niada-9b8a7222a">https://www.linkedin.com/in/adama-niada-9b8a7222a</a></span>
+                            <span><a class="underline:none mx-3" href="https://www.facebook.com/adama.niada.142">https://www.facebook.com/adama.niada.142</a></span>
+                        </div>
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                        <h4 class="alert-heading">Profil</h4>
+                        <p>
+                            Je cherche un emploi d'informaticien, de
+                            développeur et ou de concepteur web ou tout autre
+                            emploi dans le secteur de l’informatique. Mon
+                            objectif est de développer mes compétences sur
+                            ces métiers dans le but d’évoluer sur des postes
+                            clefs pour la suite de ma carrière.
+                        </p>
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                        <h4 class="alert-heading">Well done!</h4>
+                        <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+                        <hr>
+                        <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                        <h4 class="alert-heading">Well done!</h4>
+                        <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+                        <hr>
+                        <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+                    </div>
+                    <div class="alert alert-light" role="alert">
+                        <h4 class="alert-heading">Well done!</h4>
+                        <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+                        <hr>
+                        <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+                    </div>
                 </div>
             </div>
         </div>
